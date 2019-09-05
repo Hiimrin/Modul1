b42252759b86576cedb13f3070455a92d71efb5b
Hiimrin <hiiimrin@gmail.com>
2019-09-05 09:51:25 +0700

    Sesion1

---
+++ thtoantuu.js
@@ -0,0 +1,8 @@
+let Width = prompt(parseInt("Enter Width"));
+let Height = prompt(parseInt("EnterHeight"));
+let area = Width * Height;
+document.write("Height = " +Height);
+document.write("Width =" +Width);
+document.write( "The area = " + area);
+
+
